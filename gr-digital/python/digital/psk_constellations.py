c1c513dff86f9df12fb276d1f8e5224b102fd0d1
gvanhoy <gmvanhoy@gmail.com>
2019-10-06 17:00:27 -0400

    Re-formatting for better extensibility, readability.

--- gr-digital/python/digital/psk_constellations.py
+++ gr-digital/python/digital/psk_constellations.py
@@ -22,7 +22,7 @@
 
 from __future__ import absolute_import
 from __future__ import unicode_literals
-import numpy
+import numpy as np
 
 '''
 Note on the naming scheme. Each constellation is named using a prefix
@@ -63,234 +63,28 @@ we always select one rotation as our basic rotation that the other
 rotations are based off of.
 '''
 
-# BPSK Constellation symbols
-def psk_2_0x0():
-    '''
-    0 | 1
-    '''
-    constellation_points = [
-        -1, 1
-    ]
-    symbols = [
-        0, 1
-    ]
-    constellation_points, symbols = constellation_map_generator(
-        constellation, 
-        0, 
-        0
-    )
-    return digital.constellation_sector(
-        constellation_points,
-        symbols,
-        1,  # rotational symmetry
-        1,  # dimensionality
-        2   # sectors
-    ).base()
+def psk_constellation(const_points, symbols, k, pi):
+    const_points, symbols = constellation_map_generator(const_points, symbols, k, pi)
+    return digital.constellation_psk(constellation_points, symbols, len(const_points)).base() 
 
-psk_2 = psk_2_0x0  # Basic BPSK rotation
-psk_2_0 = psk_2    # First ID for BPSK rotations
+def const_bpsk(k, pi):
+    constellation_points = np.exp(2j * np.pi * np.linspace(0, 1, 2) / 2.0)
+    symbols = [0, 1]
+    return psk_constellation(constellation_points, symbols, k, pi)
 
-def psk_2_0x1():
-    '''
-    1 | 0
-    '''
-    constellation_points = [
-        -1, 1
-    ]
-    symbols = [
-        0, 1
-    ]
-    constellation_points, symbols = constellation_map_generator(
-        constellation, 
-        1, 
-        0
-    )
-    return digital.constellation_sector(
-        constellation_points,
-        symbols,
-        1,  # rotational symmetry
-        1,  # dimensionality
-        2   # sectors
-    ).base()
+def const_qpsk(k, pi):
+    constellation_points = np.exp(2j * np.pi * np.linspace(0, 3, 4) / 4.0)
+    symbols = [0, 1, 2, 3]
+    return psk_constellation(constellation_points, symbols, k, pi)
 
-psk_2_1 = psk_2_0x1
+def const_8psk(k, pi):
+    constellation_points = np.exp(2j * np.pi * np.linspace(0, 7, 8) / 8.0)
+    symbols = [0, 1, 3, 2, 6, 7, 5]
+    return psk_constellation(constellation_points, symbols, k, pi)
 
-############################################################
-# QPSK Constellation symbolss
-############################################################
-def psk_4_0x0_0_1():
-    '''
-    | 10 | 11
-    | -------
-    | 00 | 01
-    '''
-    constellation_points = [
-        -1 - 1j, 1 - 1j, -1 + 1j, 1 + 1j
-    ]
-    symbols = [
-        0, 1, 2, 3
-    ]
-    return digital.constellation_sector(
-        constellation_points,
-        symbols,
-        2,  # rotational symmetry
-        1,  # dimensionality
-        2   # sectors
-    ).base()
+if __name__ == "__main__":
+    from gnuradio import digital
+    from constellation_map_generator import constellation_map_generator
 
-psk_4 = psk_4_0x0_0_1
-psk_4_0 = psk_4
-
-def psk_4_0x1_0_1():
-    '''
-    | 11 | 10
-    | -------
-    | 01 | 00
-    '''
-    constellation_points = [
-        -1 - 1j, 1 - 1j, -1 + 1j, 1 + 1j
-    ]
-    symbols = [
-        1, 0, 3, 2
-    ]
-    return digital.constellation_sector(
-        constellation_points,
-        symbols,
-        2,  # rotational symmetry
-        1,  # dimensionality
-        2   # sectors
-    ).base()
-
-psk_4_1 = psk_4_0x1_0_1
-
-def psk_4_0x2_0_1():
-    '''
-    | 00 | 01
-    | -------
-    | 10 | 11
-    '''
-    constellation_points = [
-        -1 - 1j, 1 - 1j, -1 + 1j, 1 + 1j
-    ]
-    symbols = [
-        2, 3, 0, 1
-    ]
-    return digital.constellation_sector(
-        constellation_points,
-        symbols,
-        2,  # rotational symmetry
-        1,  # dimensionality
-        2   # sectors
-    ).base()
-
-psk_4_2 = psk_4_0x2_0_1
-
-def psk_4_0x3_0_1():
-    '''
-    | 01 | 00
-    | -------
-    | 11 | 10
-    '''
-    constellation_points = [
-        -1 - 1j, 1 - 1j, -1 + 1j, 1 + 1j
-    ]
-    symbols = [
-        3, 2, 1, 0
-    ]
-    return digital.constellation_sector(
-        constellation_points,
-        symbols,
-        2,  # rotational symmetry
-        1,  # dimensionality
-        2   # sectors
-    ).base()
-
-psk_4_3 = psk_4_0x3_0_1
-
-def psk_4_0x0_1_0():
-    '''
-    | 01 | 11
-    | -------
-    | 00 | 10
-    '''
-    constellation_points = [
-        -1 - 1j, 1 - 1j, -1 + 1j, 1 + 1j
-    ]
-    symbols = [
-        0, 2, 1, 3
-    ]
-    return digital.constellation_sector(
-        constellation_points,
-        symbols,
-        2,  # rotational symmetry
-        1,  # dimensionality
-        2   # sectors
-    ).base()
-
-psk_4_4 = psk_4_0x0_1_0
-
-def psk_4_0x1_1_0():
-    '''
-    | 00 | 10
-    | -------
-    | 01 | 11
-    '''
-    constellation_points = [
-        -1 - 1j, 1 - 1j, -1 + 1j, 1 + 1j
-    ]
-    symbols = [
-        1, 3, 0, 2
-    ]
-    return digital.constellation_sector(
-        constellation_points,
-        symbols,
-        2,  # rotational symmetry
-        1,  # dimensionality
-        2   # sectors
-    ).base()
-
-psk_4_5 = psk_4_0x1_1_0
-
-def psk_4_0x2_1_0():
-    '''
-    | 11 | 01
-    | -------
-    | 10 | 00
-    '''
-    constellation_points = [
-        -1 - 1j, 1 - 1j, -1 + 1j, 1 + 1j
-    ]
-    symbols = [
-        2, 0, 3, 1
-    ]
-    return digital.constellation_sector(
-        constellation_points,
-        symbols,
-        2,  # rotational symmetry
-        1,  # dimensionality
-        2   # sectors
-    ).base()
-
-psk_4_6 = psk_4_0x2_1_0
-
-def psk_4_0x3_1_0():
-    '''
-    | 10 | 00
-    | -------
-    | 11 | 01
-    '''
-    constellation_points = [
-        -1 - 1j, 1 - 1j, -1 + 1j, 1 + 1j
-    ]
-    symbols = [
-        3, 1, 2, 0
-    ]
-    return digital.constellation_sector(
-        constellation_points,
-        symbols,
-        2,  # rotational symmetry
-        1,  # dimensionality
-        2   # sectors
-    ).base()
-
-psk_4_7 = psk_4_0x3_1_0
+    print(const_bpsk(0, [0]))
+    print(const_bpsk(0, [1]))
